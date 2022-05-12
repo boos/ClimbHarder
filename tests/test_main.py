3fa6b811479a7292bb56e7b9c4676e0cefd19452
Roberto Martelloni <rmartelloni@gmail.com>
2022-05-12 17:53:02 +0200

    Renamed test that read root / path.

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,7 @@ app.include_router(security.router)
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_root():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {"message": "I am ClimbHarder, and I will make you climb HARDER!"}
