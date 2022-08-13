a787d9244649cb5a519ad11e56e64aa3dea48f1f
Roberto Martelloni <rmartelloni@gmail.com>
2022-08-13 23:35:28 +0200

    Changed from /workouts/ to /climbings/ endpoints the relevant test cases.

--- tests/test_climbings.py
+++ tests/test_climbings.py
@@ -11,7 +11,7 @@ async def test_can_successfully_add_climbing_exercise_to_workout_sent_now():
     ac = await test_user_login()
 
     data = {"grade": "6a", "sent": True}
-    response = await ac.post('/workouts/now', content=json.dumps(data))
+    response = await ac.post('/climbings/now', content=json.dumps(data))
     assert response.json()['load'] == 6.0
     assert response.status_code == 201
     assert data.items() <= response.json().items()
@@ -22,7 +22,7 @@ async def test_can_successfully_add_climbing_exercise_to_workout_no_sent_now():
     ac = await test_user_login()
 
     data = {"grade": "6a", "moves": 1, "total_moves": 10, "sent": False}
-    response = await ac.post('/workouts/now', content=json.dumps(data))
+    response = await ac.post('/climbings/now', content=json.dumps(data))
     assert response.status_code == 201
     assert response.json()['load'] == 0.6
     assert data.items() <= response.json().items()
@@ -33,7 +33,7 @@ async def test_can_successfully_add_climbing_exercise_to_workout_sent_when():
     ac = await test_user_login()
 
     data = {"grade": "4a", "sent": True}
-    response = await ac.post('/workouts/2022/01/01/01/01/01', content=json.dumps(data))
+    response = await ac.post('/climbings/2022/01/01/01/01/01', content=json.dumps(data))
     assert response.status_code == 201
     assert response.json()['load'] == 4
     assert response.json()['when'].__str__() == datetime.datetime(2022, 1, 1, 1, 1, 1).isoformat()
@@ -45,7 +45,7 @@ async def test_can_successfully_add_climbing_exercise_to_workout_no_sent_when():
     ac = await test_user_login()
 
     data = {"grade": "4a", "moves": 1, "total_moves": 10, "sent": False}
-    response = await ac.post('/workouts/2022/01/01/01/01/02', content=json.dumps(data))
+    response = await ac.post('/climbings/2022/01/01/01/01/02', content=json.dumps(data))
     assert response.status_code == 201
     assert response.json()['load'] == 0.4
     assert response.json()['when'].__str__() == datetime.datetime(2022, 1, 1, 1, 1, 2).isoformat()
