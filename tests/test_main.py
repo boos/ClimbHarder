cc2841acd8e8b8e93a52351286e2e9b8ceeed3f4
Roberto Martelloni <rmartelloni@gmail.com>
2023-10-30 21:57:34 +0100

    Test case to check root page.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
+import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
 from main import app
 from routers import users, security
@@ -6,10 +8,12 @@ from routers import users, security
 app.include_router(users.router)
 app.include_router(security.router)
 
-client = TestClient(app)
+client = TestClient(app, base_url="http://127.0.0.1:8000")
 
 
-def test_read_root():
-    response = client.get('/')
+@pytest.mark.anyio
+async def test_read_root():
+    ac = AsyncClient(app=app, base_url="http://127.0.0.1:8000")
+    response = await ac.get('/')
     assert response.status_code == 200
     assert response.json() == {"message": "I am ClimbHarder, and I will make you climb HARDER!"}
