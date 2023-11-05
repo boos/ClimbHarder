49037c9ec4af7c70bf95add1558fcb5089b795a5
Roberto Martelloni <rmartelloni@gmail.com>
2023-11-05 23:12:23 +0100

    Loading .env as environment variables before running using dotenv.

--- tests/shared.py
+++ tests/shared.py
@@ -1,6 +1,8 @@
 from httpx import AsyncClient
 from main import app
+from dotenv import load_dotenv
 
+load_dotenv()
 
 async def test_user_login(username='test', password='test') -> AsyncClient:
     ac = AsyncClient(app=app, base_url="http://127.0.0.1:8000")
